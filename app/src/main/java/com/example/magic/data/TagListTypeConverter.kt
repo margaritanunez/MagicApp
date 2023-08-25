6223d4d1a12996dd88864fdb1b352a5a7691edfb
Margaret <maenunezt@gmail.com>
2023-08-25 16:07:11 -0400

    explicación de un fragmento en RetrofitClient.kt y se añade TagListTypeConverter.

---
+++ app/src/main/java/com/example/magic/data/TagListTypeConverter.kt
@@ -0,0 +1,46 @@
+package com.example.magic.data
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+
+class TagListTypeConverter {
+
+    @TypeConverter
+    fun fromListToString(list: List<String>) = Gson().toJson(list)
+
+    @TypeConverter
+    fun jsonToList(value: String) = Gson().fromJson(value, Array<String>::class.java).toList()
+
+}
+
+/*
+El código que has compartido es una clase llamada TagListTypeConverter, que probablemente esté
+diseñada para ser utilizada en el contexto de la arquitectura de bases de datos y
+Room Persistence Library en Android. Esta clase se utiliza para convertir entre una lista de cadenas
+(strings) y su representación en formato JSON utilizando la biblioteca Gson.
+La función de conversión se utiliza para permitir que Room almacene y recupere objetos complejos
+ como listas en una base de datos SQLite.
+
+class TagListTypeConverter: Se define una clase llamada TagListTypeConverter.
+@TypeConverter: Esto indica que los métodos en esta clase son convertidores de tipo utilizados por
+Room.
+fun fromListToString(list: List<String>): Esta función recibe una lista de cadenas y la convierte en
+una representación JSON utilizando la biblioteca Gson. En otras palabras, toma una lista de cadenas
+y la convierte en una cadena JSON. Esta función probablemente se utilizará cuando se quiera
+almacenar una lista de cadenas en la base de datos.
+
+fun jsonToList(value: String): Esta función recibe una cadena en formato JSON y la convierte de
+nuevo en una lista de cadenas utilizando la biblioteca Gson. La cadena JSON representa una lista de
+cadenas, y esta función convierte esa cadena en una lista de cadenas de nuevo. Esta función
+probablemente se utilizará cuando se quiera recuperar una lista de cadenas de la base de datos.
+Gson(): Se crea una instancia de la clase Gson, que es una biblioteca de serialización y
+deserialización JSON utilizada comúnmente en el ecosistema de Android.
+toJson(list): Utiliza la instancia de Gson para convertir la lista de cadenas en una cadena JSON.
+fromJson(value, Array<String>::class.java).toList(): Utiliza la instancia de Gson para convertir la
+cadena JSON en un array de cadenas (Array<String>) y luego convierte ese array en una lista de
+cadenas.
+
+En resumen, esta clase se utiliza para convertir listas de cadenas en representaciones JSON y
+viceversa utilizando la biblioteca Gson. Esto permite que las listas de cadenas se almacenen y
+recuperen de una base de datos SQLite utilizando Room.
+ */
--- app/src/main/java/com/example/magic/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/magic/data/remote/RetrofitClient.kt
@@ -17,6 +17,35 @@ class RetrofitClient {
 
              */
 
+            /*Utiliza la biblioteca OkHttpClient para crear una instancia de un cliente HTTP.
+            Este cliente HTTP se configura con ciertos parámetros de tiempo de espera
+            y luego se construye para estar listo para realizar solicitudes a servidores remotos.
+            var okHttpClient: se declara una variable que se utiliza para almacenar la instancia
+            del cliente HTTP que estamos construyendo.
+            OkHttpClient.Builder(): Se crea una instancia de OkHttpClient.Builder, que es un
+            constructor de objetos utilizado para configurar y construir una instancia de
+            OkHttpClient.
+            .connectTimeout(1, TimeUnit.MINUTES): Se configura el tiempo máximo que el cliente
+            intentará conectarse a un servidor remoto. En este caso, se establece un tiempo de
+            espera de 1 minuto para la conexión utilizando la unidad de tiempo TimeUnit.MINUTES.
+            .readTimeout(30, TimeUnit.SECONDS): Se configura el tiempo máximo que el cliente
+            esperará para leer datos de la respuesta del servidor. Aquí, se establece un tiempo de
+            espera de 30 segundos para la lectura de datos utilizando la unidad de tiempo
+            TimeUnit.SECONDS.
+            .writeTimeout(30, TimeUnit.SECONDS): Se configura el tiempo máximo que el cliente
+            esperará para escribir datos en la solicitud al servidor. Igual que el tiempo de lectura,
+            se establece un tiempo de espera de 30 segundos para la escritura de datos utilizando la
+            unidad de tiempo TimeUnit.SECONDS.
+            .build(): Una vez que se han configurado todos los parámetros del cliente HTTP mediante
+            los métodos anteriores, se llama al método build() para crear una instancia final de
+            OkHttpClient con la configuración proporcionada.
+
+            En resumen, este código crea una instancia de OkHttpClient y la configura con tiempos
+            máximos de espera para la conexión, la lectura y la escritura en las solicitudes y
+            respuestas de los servidores remotos. Este cliente HTTP puede luego ser utilizado para
+            realizar solicitudes HTTP, como obtener recursos desde un servidor, enviar datos, etc.
+             */
+
             fun getRetrofitPhone() : MagicApi{
                 val mRetrofit = Retrofit.Builder().baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create()).build()
